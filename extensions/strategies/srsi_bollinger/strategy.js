42408d0d9885646d1b7315ca921bc85cb0c3dc5c
Michael Sutton <station384@gmail.com>
2018-06-04 13:25:48 -0700

    Use StochRSI from TA-Lib

--- extensions/strategies/srsi_bollinger/strategy.js
+++ extensions/strategies/srsi_bollinger/strategy.js
@@ -1,9 +1,8 @@
 let z = require('zero-fill')
   , n = require('numbro')
-  , srsi = require('../../../lib/srsi')
+  , ta_srsi = require('../../../lib/ta_srsi')
   , bollinger = require('../../../lib/bollinger')
   , Phenotypes = require('../../../lib/phenotype')
-var last_period = ''
 module.exports = {
   name: 'srsi_bollinger',
   description: 'Stochastic BollingerBand Strategy',
@@ -13,13 +12,13 @@ module.exports = {
     this.option('period_length', 'period length, same as --period', String, '5m')
     this.option('min_periods', 'min. number of history periods', Number, 200)
     this.option('rsi_periods', 'number of RSI periods', 14)
-    this.option('srsi_periods', 'number of RSI periods', Number, 9)
     this.option('srsi_k', '%D line', Number, 5)
     this.option('srsi_d', '%D line', Number, 3)
     this.option('srsi_k_sell', 'K must be above this before selling', Number, 80)
     this.option('srsi_k_buy', 'K must be below this before buying', Number, 10)
+    this.option('srsi_dType','D type mode', String, 'SMA'),
 
-
+    //'SMA','EMA','WMA','DEMA','TEMA','TRIMA','KAMA','MAMA','T3'
     
     
     this.option('bollinger_size', 'period size', Number, 10)
@@ -29,79 +28,81 @@ module.exports = {
 
   },
 
-  calculate: function (s) {
-    // compute Stochastic RSI
-    // if (s.options.srsi_periods > s.options.rsi_periods) {s.options.srsi_periods = s.options.rsi_periods}
-    if (s.options.srsi_k < s.options.srsi_d) {s.options.srsi_k = s.options.srsi_d}
-    if (last_period == s.period.period_id) return
-    bollinger(s, 'bollinger', s.options.bollinger_size)
-    srsi(s, 'srsi', s.options.rsi_periods, s.options.srsi_k, s.options.srsi_d)
-    last_period = s.period.period_id
-    if (s.in_preroll) return
-    var divergent = s.period.srsi_K - s.period.srsi_D 
-    var divergent1 = s.lookback[0].divergent
-    var last_divergent = s.lookback[0].srsi_K - s.lookback[0].srsi_D
-    var _switch = 0//s.lookback[0]._switch
-    var nextdivergent = (( divergent + divergent1 ) /2) + (divergent - divergent1) 
-    if ((last_divergent <= 0 && (divergent > 0)) ) _switch = 1 // price rising 
-    if ((last_divergent >= 0 && (divergent < 0)) ) _switch = -1 // price falling
-    s.period.divergent = divergent
-    s.period._switch = _switch
-    s.period.nextdivergent = nextdivergent
-    //s.period.recommendBidPricePct =  ((nextdivergent)/Math.E )
+  calculate: function () {
+
 
 
   },
 
   onPeriod: function (s, cb) {
     //make sure we have all values
-
-
-
-
+    // compute Stochastic RSI
     if (s.in_preroll) return cb()
-    if (s.period.bollinger && s.period.srsi_D && s.period.srsi_K) {
-      if (s.period.bollinger.upper && s.period.bollinger.lower) {
-        let upperBound = s.period.bollinger.upper[s.period.bollinger.upper.length-1]
-        let lowerBound = s.period.bollinger.lower[s.period.bollinger.lower.length-1]
-        //let midBound = s.period.bollinger.mid[s.period.bollinger.upper.length-1]
-        var divergent =  s.period.divergent//s.divergent
-        var nextdivergent = s.period.nextdivergent
-        var _switch = s.period._switch//s._switch 
-        // s.options.markdown_buy_pct = s.period.recommendBidPricePct
-        // s.options.markup_sell_pct = s.period.recommendBidPricePct
+    bollinger(s, 'bollinger', s.options.bollinger_size)
+    ta_srsi(s, 'srsi', s.options.rsi_periods, s.options.srsi_k, s.options.srsi_d, s.options.srsi_dType).
+      then(function(inres) {
+    
+
+        var divergent = inres.outFastK[inres.outFastK.length-1] - inres.outFastD[inres.outFastD.length-1]
+        s.period.srsi_D = inres.outFastD[inres.outFastD.length-1]
+        s.period.srsi_K = inres.outFastK[inres.outFastK.length-1]
+        var divergent1 = s.lookback[0].divergent
+        var last_divergent = inres.outFastK[inres.outFastK.length-2] - inres.outFastD[inres.outFastD.length-2]
+        var _switch = 0//s.lookback[0]._switch
+        var nextdivergent = (( divergent + divergent1 ) /2) + (divergent - divergent1) 
+        if ((last_divergent <= 0 && (divergent > 0)) ) _switch = 1 // price rising 
+        if ((last_divergent >= 0 && (divergent < 0)) ) _switch = -1 // price falling
+        s.period.divergent = divergent
+        s.period._switch = _switch
+        s.period.nextdivergent = nextdivergent
+
+
+
+        if (s.in_preroll) return cb()
+        if (s.period.bollinger && inres.outFastD[inres.outFastD.length-1] && inres.outFastK[inres.outFastK.length-1]) {
+          if (s.period.bollinger.upper && s.period.bollinger.lower) {
+            let upperBound = s.period.bollinger.upper[s.period.bollinger.upper.length-1]
+            let lowerBound = s.period.bollinger.lower[s.period.bollinger.lower.length-1]
+            //let midBound = s.period.bollinger.mid[s.period.bollinger.upper.length-1]
+            //   var divergent =  s.period.divergent//s.divergent
+            //    var nextdivergent = s.period.nextdivergent
+            //  var _switch = s.period._switch//s._switch 
+            // s.options.markdown_buy_pct = s.period.recommendBidPricePct
+            // s.options.markup_sell_pct = s.period.recommendBidPricePct
   
   
-        // K is fast moving
+            // K is fast moving
   
       
-        if (_switch != 0  ) // && Math.abs(divergent) >=1
-        {
-          if (s.period.close > ((upperBound / 100) * (100 - s.options.bollinger_upper_bound_pct)) && nextdivergent <= divergent && _switch == -1 && s.period.srsi_K > s.options.srsi_k_sell) 
-          // if (s.period.close >=  midBound   && nextdivergent <= divergent  && s.period.srsi_K > s.options.srsi_k_sell  )  //&& divergent >= 1 
-          {
-            // if(s.my_trades.length > 0)
-            // if(s.my_trades[s.my_trades.length-1].price < s.period.close) 
-            // if ((s.period.close > s.last_buy_price) || ( s.period.close / s.last_buy_price  )  > 1.5 )
-            s.signal = 'sell'
-          } 
-          // if ((s.period.close < (lowerBound / 100) * (100 + s.options.bollinger_lower_bound_pct)) && _switch == -1 && s.period.srsi_K < s.options.srsi_k_buy) 
-          if (s.period.close <= ((lowerBound / 100) * (100 + s.options.bollinger_lower_bound_pct))  && nextdivergent >= divergent  && _switch == 1    && s.period.srsi_K < s.options.srsi_k_buy) // && divergent <= -1
-          //if (s.period.close <= lowerBound   && nextdivergent >= divergent     && s.period.srsi_K < s.options.srsi_k_buy) // && divergent <= -1
+            if (_switch != 0  ) // && Math.abs(divergent) >=1
+            {
+              if (s.period.close > ((upperBound / 100) * (100 - s.options.bollinger_upper_bound_pct)) && nextdivergent <= divergent && _switch == -1 && s.period.srsi_K > s.options.srsi_k_sell) 
+              // if (s.period.close >=  midBound   && nextdivergent <= divergent  && s.period.srsi_K > s.options.srsi_k_sell  )  //&& divergent >= 1 
+              {
+                // if(s.my_trades.length > 0)
+                // if(s.my_trades[s.my_trades.length-1].price < s.period.close) 
+                // if ((s.period.close > s.last_buy_price) || ( s.period.close / s.last_buy_price  )  > 1.5 )
+                s.signal = 'sell'
+              } 
+              // if ((s.period.close < (lowerBound / 100) * (100 + s.options.bollinger_lower_bound_pct)) && _switch == -1 && s.period.srsi_K < s.options.srsi_k_buy) 
+              if (s.period.close <= ((lowerBound / 100) * (100 + s.options.bollinger_lower_bound_pct))  && nextdivergent >= divergent  && _switch == 1    && s.period.srsi_K < s.options.srsi_k_buy) // && divergent <= -1
+              //if (s.period.close <= lowerBound   && nextdivergent >= divergent     && s.period.srsi_K < s.options.srsi_k_buy) // && divergent <= -1
   
-          {
-            s.signal = 'buy'
-          } 
+              {
+                s.signal = 'buy'
+              } 
           
-        }
-        else {
-          s.signal = null // hold
-        }
+            }
+            else {
+              s.signal = null // hold
+            }
   
-      }
-    }
-    cb()
+          }
+        }
+        cb()
+      })
   },
+
   onReport: function (s) {
     var cols = []
     if (s.period.bollinger) {
@@ -132,8 +133,8 @@ module.exports = {
   phenotypes: 
         {
           // -- common
-          period_length: Phenotypes.ListOption(['1m', '5m', '10m','15m','30m','45m','60m']),
-          min_periods: Phenotypes.Range(1, 200),
+          period_length: Phenotypes.ListOption(['1m', '2m', '3m', '4m', '5m']),//, '10m','15m','30m','45m','60m'
+          min_periods: Phenotypes.Range(10, 52),
           markdown_buy_pct: Phenotypes.RangeFactor(-1.0, 1.0, 0.1),
           markup_sell_pct: Phenotypes.RangeFactor(-1.0, 1.0, 0.1),
           order_type: Phenotypes.ListOption(['maker', 'taker']),
@@ -144,11 +145,12 @@ module.exports = {
 
           // -- strategy
           rsi_periods: Phenotypes.Range(2, 20),
-          srsi_periods: Phenotypes.Range(2, 20),
-          srsi_k: Phenotypes.Range(1, 50),
-          srsi_d: Phenotypes.Range(1, 50),
+          srsi_k: Phenotypes.Range(1, 10),
+          srsi_d: Phenotypes.Range(1, 10),
           srsi_k_sell: Phenotypes.RangeFactor(60.0, 90.0, 1.0),
           srsi_k_buy: Phenotypes.RangeFactor(0.0, 40.0, 1.0),
+          srsi_dType:  Phenotypes.ListOption(['SMA','EMA','WMA','DEMA','TEMA','TRIMA','KAMA','MAMA','T3']),
+
 
 
           bollinger_size: Phenotypes.RangeFactor(10, 25, 1),
