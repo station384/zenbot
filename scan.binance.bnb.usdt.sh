d81b4b7b16a4946a202d2fa5770e7a68b4dbb3e7
Michael Sutton <station384@gmail.com>
2018-06-22 14:10:50 -0400

    syncing my play settings that are working

--- scan.binance.bnb.usdt.sh
+++ scan.binance.bnb.usdt.sh
@@ -1,3 +1,3 @@
-./scripts/genetic_backtester/darwin.js --selector=binance.bnb-usdt  --keep_lookback_periods=200 --asset_capital=0.0 --fitnessCalcType=profit --currency_capital=45 --use_strategies=station  --population=100  --days=15  --maxCores=6  --generateLaunch   --ignoreLaunchFitness   --minTrades=5
+./scripts/genetic_backtester/darwin.js --selector=binance.bnb-usdt  --keep_lookback_periods=200 --asset_capital=0.0 --fitnessCalcType=profit --currency_capital=45 --use_strategies=station  --population=20  --days=15  --maxCores=6  --generateLaunch   --ignoreLaunchFitness   --minTrades=25
 #--srsi_k_sell=80 --srsi_k_buy=20 --bollinger_size=21 --bollinger_time=2 --srsi_k=5 --srsi_d=3
 
